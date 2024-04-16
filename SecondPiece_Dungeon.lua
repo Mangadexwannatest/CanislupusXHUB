425ccfef1cca7526109fa42b53473f9f6c22e0d9
Mangadexwannatest <110204929+Mangadexwannatest@users.noreply.github.com>
2024-04-16 14:49:01 +0700

    Update SecondPiece_Dungeon.lua

--- SecondPiece_Dungeon.lua
+++ SecondPiece_Dungeon.lua
@@ -828,16 +828,16 @@ end))
             local script = 
             [[
                 repeat wait() until game:IsLoaded()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/LL"))()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/SecondPiece.lua"))()
         
             local success = pcall(function()
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/LL"))()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/SecondPiece.lua"))()
              end)
              
              print(success)
              if not success then
                 wait(20)
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/LL"))()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/SecondPiece.lua"))()
              end
             ]]
             if State == Enum.TeleportState.InProgress and Options.AutoExecuteScript.Value then
