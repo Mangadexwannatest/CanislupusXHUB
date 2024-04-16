fac572c36f1dcee96e7633d5ca53713ccb0198d7
Mangadexwannatest <110204929+Mangadexwannatest@users.noreply.github.com>
2024-04-16 14:55:33 +0700

    Update SecondPiece_MainGame.lua

--- SecondPiece_MainGame.lua
+++ SecondPiece_MainGame.lua
@@ -1776,20 +1776,29 @@
                 end
             end)
         end))
-
-    coroutine.resume(coroutine.create(function()
-    game.Players.LocalPlayer.OnTeleport:Connect(function(State)
-        local QueueOnTeleport = queue_on_teleport or queueonteleport or (syn and syn.queue_on_teleport)
-        local script = 
-        [[
-            repeat wait() until game:IsLoaded()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/CanisLupusXL/CanislupusXHub/main/SecondPieceTestTrail.lua'))()
-        ]]
-        if State == Enum.TeleportState.InProgress and Options.AutoExecuteScript.Value then
-            QueueOnTeleport(script)
-            end
-    end)
-    end))
+        coroutine.resume(coroutine.create(function()
+            game.Players.LocalPlayer.OnTeleport:Connect(function(State)
+                local QueueOnTeleport = queue_on_teleport or queueonteleport or (syn and syn.queue_on_teleport)
+                local script = 
+                [[
+                    repeat wait() until game:IsLoaded()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/SecondPiece.lua"))()
+            
+                local success = pcall(function()
+                    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/SecondPiece.lua"))()
+                 end)
+                 
+                 print(success)
+                 if not success then
+                    wait(20)
+                    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/SecondPiece.lua"))()
+                 end
+                ]]
+                if State == Enum.TeleportState.InProgress and Options.AutoExecuteScript.Value then
+                    QueueOnTeleport(script)
+                    end
+            end)
+            end))
 
 
         end -- End Of If
