4e398111d14ef229c8940ee00648623ab147d3d7
Mangadexwannatest <110204929+Mangadexwannatest@users.noreply.github.com>
2024-04-26 06:27:20 +0700

    Update SecondPiece_MainGame.lua

--- SecondPiece_MainGame.lua
+++ SecondPiece_MainGame.lua
@@ -602,11 +602,11 @@ end
 
 coroutine.resume(coroutine.create(function()
     while task.wait() do pcall(function()
-        if not FindPlayerSkill() then
+        if FindPlayerSkill() then
         else
             if Dist() <= 10 then
             game.Workspace.Lives:WaitForChild(game.Players.LocalPlayer.Name):WaitForChild("Humanoid")
-            wait(.5)
+           task.wait(0.5)
     game:GetService'VirtualUser':CaptureController()
     game:GetService'VirtualUser':Button1Down(Vector2.new(1200,672))
         end
