c72e9d7e13050f91971190309732ab0d9167f437
Mangadexwannatest <110204929+Mangadexwannatest@users.noreply.github.com>
2024-05-16 08:07:23 +0700

    Update All_Start_Tower_Defense.lua

--- All_Start_Tower_Defense.lua
+++ All_Start_Tower_Defense.lua
@@ -1530,14 +1530,18 @@ if game:WaitForChild("CoreGui"):FindFirstChild("CrazyDay") == nil then
                                 end)
                             end)
 
+                            local function excutescript()
+                                local QueueOnTeleport = queue_on_teleport or queueonteleport or (syn and syn.queue_on_teleport)
+                                QueueOnTeleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/All_Start_Tower_Defense.lua'))()")
+                            end
+
                             spawn(function ()
                                 game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(Kick)
                                     if ((Kick.Name == "ErrorPrompt") and Kick:FindFirstChild("MessageArea") and Kick.MessageArea:FindFirstChild("ErrorFrame")) then
                                         repeat wait() until Options.AutoRejoinError.Value
-                                        task.wait(7.5)
-                                        local QueueOnTeleport = queue_on_teleport or queueonteleport or (syn and syn.queue_on_teleport)
-                                        QueueOnTeleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/All_Start_Tower_Defense.lua'))()")
+                                        task.wait(5.5)
                                         game:GetService("TeleportService"):Teleport(4996049426)
+                                        excutescript()
                                 end
                             end)
                         end)
@@ -1553,11 +1557,8 @@ if game:WaitForChild("CoreGui"):FindFirstChild("CrazyDay") == nil then
                         spawn(function ()
                             game.Players.LocalPlayer.OnTeleport:Connect(function(state)
                                 getgenv().OnTeleport = true
-                                local QueueOnTeleport = queue_on_teleport or queueonteleport or (syn and syn.queue_on_teleport)
                                 if state == Enum.TeleportState.InProgress and Options.AutoExecuteScript.Value then
-                                    QueueOnTeleport(
-                                        "loadstring(game:HttpGet('https://raw.githubusercontent.com/Mangadexwannatest/CanislupusXHUB/main/All_Start_Tower_Defense.lua'))()"
-                                    )
+                                    excutescript()
                                 end
                             end)
                         end)
